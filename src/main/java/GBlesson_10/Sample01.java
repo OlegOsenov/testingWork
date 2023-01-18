97cdb0a532dfdec6f68998b6ea72f4af70566ba1
Oleg Osenov <olejka_oon@mail.ru>
2023-01-18 18:41:50 +0300

    Тест задач

---
+++ src/main/java/GBlesson_10/Sample01.java
@@ -0,0 +1,13 @@
+package GBlesson_10;
+
+import java.util.ArrayList;
+
+public class Sample01 {
+
+    public static void main(String[] args) {
+
+        ArrayList<Cat> cats = new ArrayList<>();
+
+    }
+
+}
