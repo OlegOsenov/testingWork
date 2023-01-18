1ae197c2b7473821680147336a5696ca93063a53
Oleg Osenov <olejka_oon@mail.ru>
2023-01-18 19:12:02 +0300

    Интерфейс SET добавил элементы  HashSet

--- src/main/java/GBlesson_10/Sample02.java
+++ src/main/java/GBlesson_10/Sample02.java
@@ -1,10 +1,40 @@
 package GBlesson_10;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
 public class Sample02 {
 
     public static void main(String[] args) {
 
+        Cat cat01 = new Cat("Персик");
+        HashSet<Cat> cats = new HashSet<>();
+        cats.add(cat01);
+        cats.add(cat01);
+        cats.add(new Cat("Тиграша"));
+        cats.add(new Cat("Мурзик"));
+
+
+        // printList(cats);
+
+        printHashSet(cats);
+
+        System.out.println("Размер коллекции " + cats.size());
+
+        cats.remove(cat01);
+        System.out.println("---------------");
+
+        printHashSet(cats);
 
 
     }
+
+    private static void printHashSet(HashSet<Cat> cats) {
+        for (Cat cat : cats) {
+            System.out.println(cat.getName());
+        }
+    }
+
+
 }
+
