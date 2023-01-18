27482726d8da1358e8fd28ddb0a5ddcde30d0aa3
Oleg Osenov <olejka_oon@mail.ru>
2023-01-18 18:56:03 +0300

    Создал коллекцию Cat и вывел их имена на экран

--- src/main/java/GBlesson_10/Sample01.java
+++ src/main/java/GBlesson_10/Sample01.java
@@ -6,7 +6,18 @@ public class Sample01 {
 
     public static void main(String[] args) {
 
+        Cat cat01 = new Cat("Персик");
+
         ArrayList<Cat> cats = new ArrayList<>();
+
+        cats.add(cat01);
+        cats.add(cat01);
+        cats.add(new Cat("Тиграша"));
+        cats.add(1,new Cat("Мурзик"));
+
+        for (Cat cat: cats) {
+            System.out.println(cat.getName());
+        }
 
     }
 
