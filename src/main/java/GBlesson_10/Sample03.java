848313ef23e5f364e01c250401a6e9a84dacbb5c
Oleg Osenov <olejka_oon@mail.ru>
2023-01-18 19:25:11 +0300

    LinkedList добавил элементы и метод печати в консоль

--- src/main/java/GBlesson_10/Sample03.java
+++ src/main/java/GBlesson_10/Sample03.java
@@ -1,4 +1,36 @@
 package GBlesson_10;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Sample03 {
+    public static void main(String[] args) {
+
+        Cat cat01 = new Cat("Персик");
+        LinkedList<Cat> cats = new LinkedList<>();
+        cats.add(cat01);
+        cats.add(cat01);
+        cats.add(new Cat("Тиграша"));
+        cats.add(1,new Cat("Мурзик"));
+
+        printList(cats);
+
+        System.out.println("Размер коллекции " + cats.size());
+
+        cats.remove(cat01);
+        System.out.println("---------------");
+        printList(cats);
+
+
+
+
+    }
+
+
+    private static void printList(LinkedList<Cat> cats) {
+        for (Cat cat: cats) {
+            System.out.println(cat.getName());
+        }
+    }
+
 }
