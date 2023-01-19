3fdeaed7cf3c7103798a1d2c986b924c0b4e48b4
Oleg Osenov <olejka_oon@mail.ru>
2023-01-19 21:22:59 +0300

    Лекция 10 работа с Collection

---
+++ src/main/java/CollectionCB_10/CollectionsExample.java
@@ -0,0 +1,16 @@
+package CollectionCB_10;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollectionsExample {
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+        list.add("Januare");
+        list.add("February");
+        list.add("March");
+        System.out.println(list);
+
+    }
+}
