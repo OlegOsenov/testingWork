ffde0205d524466074dde980616017e5cf9c4e58
Oleg Osenov <olejka_oon@mail.ru>
2023-01-22 21:45:49 +0500

    Бинарный поиск -Двоичный поиск

--- src/main/java/BinarySearch/BinarySearch.java
+++ src/main/java/BinarySearch/BinarySearch.java
@@ -1,4 +1,28 @@
 package BinarySearch;
 
 public class BinarySearch {
+    public static void main(String[] args) {
+
+
+
+
+    }
+    /*
+        Просто перебираем, пока не найдет.
+        Если ничего не найдём, вернём -1
+     */
+    public static int bruteForce(double[] array, double key) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == key)
+                return i;
+        }
+        return -1;
+    }
+
+    /*
+        Двоичный поиск
+     */
+    public static int binarySearchRecursively(double[] sortedArray, double key) {
+        return binarySearchRecursively(sortedArray, key, 0, sortedArray.length);
+    }
 }
