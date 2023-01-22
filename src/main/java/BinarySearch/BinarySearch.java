506c055e6b3887e19e413c0042fb17ed87039c10
Oleg Osenov <olejka_oon@mail.ru>
2023-01-22 21:51:03 +0500

    Бинарный поиск -// Конец

--- src/main/java/BinarySearch/BinarySearch.java
+++ src/main/java/BinarySearch/BinarySearch.java
@@ -1,9 +1,20 @@
 package BinarySearch;
 
+import java.util.Arrays;
+
 public class BinarySearch {
     public static void main(String[] args) {
 
+        double[] array = generateRandomArray(100000000);
+        Arrays.sort(array); // нужно сначала отсортировать
 
+        long time = System.currentTimeMillis(); // текущее время, unix-time
+        bruteForce(array, 0.5);
+        System.out.println(System.currentTimeMillis() - time);
+
+        time = System.currentTimeMillis();
+        binarySearchRecursively(array, 0.5);
+        System.out.println(System.currentTimeMillis() - time);
 
 
     }
