a7a961acbff2baa9acd71919209fc7b4b627ddca
Oleg Osenov <olejka_oon@mail.ru>
2023-01-23 21:47:05 +0500

    вспомогательный метод

--- src/main/java/rootEquation/TheRootEquation.java
+++ src/main/java/rootEquation/TheRootEquation.java
@@ -3,5 +3,14 @@ package rootEquation;
 public class TheRootEquation {
     public static void main(String[] args) {
 
+
+
     }
+
+    // вспомогательный метод
+    public static double func(double x){
+        return Math.cos(Math.pow(x, 5)) + Math.pow(x, 4) - 345.3 * x - 23;
+    }
+
+
 }
