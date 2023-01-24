5ad2e29f10f76187617ed390b0e5b4b8b0473d36
Oleg Osenov <olejka_oon@mail.ru>
2023-01-24 21:50:51 +0500

    Создал массив строк

--- src/main/java/SortedString/SortString.java
+++ src/main/java/SortedString/SortString.java
@@ -2,6 +2,10 @@ package SortedString;
 
 public class SortString {
 
+    static String[] arr = {
+            "Now", "is", "the", "time", "for", "all", "good", "men",
+            "to", "come", "to", "the", "aid", "of", "their", "country"
+    };
 
     public static void main(String[] args) {
 
