0872dbdbca41e8d1bc4f04a3ee3ded34f4c60652
Oleg Osenov <olejka_oon@mail.ru>
2023-01-25 21:17:20 +0500

    Добавил элементы в список массива

--- src/main/java/ArrayListDemo/ArrayListDemo.java
+++ src/main/java/ArrayListDemo/ArrayListDemo.java
@@ -9,6 +9,14 @@ public class ArrayListDemo {
         //Создать списковый массив
         ArrayList<String> al = new ArrayList<>();
         System.out.println("Начальный размер al: " + al.size());
+        //Добавить элементы в список массив
+        al.add("C");
+        al.add("A");
+        al.add("E");
+        al.add("B");
+        al.add("D");
+        al.add("F");
+        al.add(1,"A2");
 
     }
 
