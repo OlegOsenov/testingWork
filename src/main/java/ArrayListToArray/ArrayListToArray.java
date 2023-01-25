2c782c51674384d76eadc2c92348e4fa62c55423
Oleg Osenov <olejka_oon@mail.ru>
2023-01-25 21:37:45 +0500

    Добавить элементы в списковый массив

--- src/main/java/ArrayListToArray/ArrayListToArray.java
+++ src/main/java/ArrayListToArray/ArrayListToArray.java
@@ -8,6 +8,13 @@ public class ArrayListToArray {
         //Преобразование ArrayList в массив
         //Создать списковый массив
         ArrayList<Integer> al = new ArrayList<>();
+        //Добавить элементы в списковый массив
+        al.add(1);
+        al.add(2);
+        al.add(3);
+        al.add(4);
+        System.out.println("Содрежимое массива al: " + al);
+
     }
 
 }
