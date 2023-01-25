3eb956f53eba99ef0fcd8be4d1d4a12a893ba58d
Oleg Osenov <olejka_oon@mail.ru>
2023-01-25 21:15:03 +0500

    Создал список массива

--- src/main/java/ArrayListDemo/ArrayListDemo.java
+++ src/main/java/ArrayListDemo/ArrayListDemo.java
@@ -1,8 +1,14 @@
 package ArrayListDemo;
 
+import java.util.ArrayList;
+
 public class ArrayListDemo {
 
     public static void main(String[] args) {
+
+        //Создать списковый массив
+        ArrayList<String> al = new ArrayList<>();
+        System.out.println("Начальный размер al: " + al.size());
 
     }
 
