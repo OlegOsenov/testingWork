1f5c362a2c8219773a3de554d7d7764d2f7c645f
Oleg Osenov <olejka_oon@mail.ru>
2023-01-25 21:35:57 +0500

    Создать списковый массив

--- src/main/java/ArrayListToArray/ArrayListToArray.java
+++ src/main/java/ArrayListToArray/ArrayListToArray.java
@@ -1,9 +1,13 @@
 package ArrayListToArray;
 
+import java.util.ArrayList;
+
 public class ArrayListToArray {
 
     public static void main(String[] args) {
         //Преобразование ArrayList в массив
+        //Создать списковый массив
+        ArrayList<Integer> al = new ArrayList<>();
     }
 
 }
