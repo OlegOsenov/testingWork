8cad8be0b76a4d1b0e373ee47dcf12ab3dc34cfd
Oleg Osenov <olejka_oon@mail.ru>
2023-01-26 12:37:37 +0500

    Создание двусторонней очереди ArrayDeque

---
+++ src/main/java/ArrayDequeDemo/ArrayDequeDemo.java
@@ -0,0 +1,16 @@
+package ArrayDequeDemo;
+
+import java.util.ArrayDeque;
+
+public class ArrayDequeDemo {
+
+    public static void main(String[] args) {
+
+        // Создать двустороннию очередь в виде массива
+        ArrayDeque<String> abq = new ArrayDeque<>();
+
+
+    }
+
+
+}
