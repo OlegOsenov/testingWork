97b0b9af9cb34fdcaedc746aa83de04d005edea9
Oleg Osenov <olejka_oon@mail.ru>
2023-01-26 13:06:55 +0500

    Добавление элементов

--- src/main/java/IteratorDerno/IteratorDemo.java
+++ src/main/java/IteratorDerno/IteratorDemo.java
@@ -1,8 +1,18 @@
 package IteratorDerno;
 
+import java.util.ArrayList;
+
 public class IteratorDemo {
 
     public static void main(String[] args) {
+
+        ArrayList<String> al = new ArrayList<>();
+        al.add("C");
+        al.add("A");
+        al.add("E");
+        al.add("B");
+        al.add("D");
+        al.add("F");
 
     }
 }
