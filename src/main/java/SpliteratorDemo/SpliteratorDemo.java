33f4bff398e24d9a513510a0bebf6a9009578d56
Oleg Osenov <olejka_oon@mail.ru>
2023-01-26 19:28:49 +0500

    Использовал tryAdvance для отображения vals

--- src/main/java/SpliteratorDemo/SpliteratorDemo.java
+++ src/main/java/SpliteratorDemo/SpliteratorDemo.java
@@ -1,7 +1,24 @@
 package SpliteratorDemo;
 
+import java.util.ArrayList;
+import java.util.Spliterator;
+
 public class SpliteratorDemo {
     public static void main(String[] args) {
+
+        ArrayList<Double> vals = new ArrayList<>();
+        vals.add(1.0);
+        vals.add(2.0);
+        vals.add(3.0);
+        vals.add(4.0);
+        vals.add(5.0);
+
+        System.out.print("Содержимое vals:\n");
+        Spliterator<Double> spliter = vals.spliterator();
+        while (spliter.tryAdvance(System.out::println));
+        System.out.println();
+
+
 
     }
 }
