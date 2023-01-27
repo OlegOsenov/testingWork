3cc37c589489019fb63cf088134dc6acd7432d48
Oleg Osenov <olejka_oon@mail.ru>
2023-01-27 20:15:28 +0500

    Создание полей и конструктора

--- src/main/java/Address/Address.java
+++ src/main/java/Address/Address.java
@@ -1,5 +1,18 @@
 package Address;
 
 public class Address {
+    private String name;
+    private String street;
+    private String city;
+    private String state;
+    private String code;
+
+    Address(String n, String s, String c, String st, String cd) {
+        name = n;
+        street = s;
+        city = c;
+        state = st;
+        code = cd;
+    }
 
 }
