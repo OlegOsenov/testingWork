1a92967e512d96012cc9f771febb8ab918ef9232
Oleg Osenov <olejka_oon@mail.ru>
2023-01-27 20:32:00 +0500

    Добавил элементы класса Address

---
+++ src/main/java/Address/MyList.java
@@ -0,0 +1,19 @@
+package Address;
+
+import java.util.LinkedList;
+
+public class MyList {
+
+    public static void main(String[] args) {
+
+        LinkedList<Address> ml = new LinkedList<>();
+        //Добавить элементы
+        ml.add(new Address("J.W. West", "11 Oak Ave",
+                            "Urbana", "IL", "61801"));
+        ml.add(new Address("Oleg Osenov", "1142 Maple Lane",
+                            "Mahomet", "IL", "61853"));
+        ml.add(new Address("Ivan Ivanov", "867 Elm St",
+                "Champing", "IL", "61820"));
+
+    }
+}
