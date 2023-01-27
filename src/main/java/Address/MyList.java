e7cf6417998f33b38ba7880ed03737824020e938
Oleg Osenov <olejka_oon@mail.ru>
2023-01-27 20:34:19 +0500

    Отобразить содержимое списка почтовых адресов

--- src/main/java/Address/MyList.java
+++ src/main/java/Address/MyList.java
@@ -9,11 +9,16 @@ public class MyList {
         LinkedList<Address> ml = new LinkedList<>();
         //Добавить элементы
         ml.add(new Address("J.W. West", "11 Oak Ave",
-                            "Urbana", "IL", "61801"));
+                "Urbana", "IL", "61801"));
         ml.add(new Address("Oleg Osenov", "1142 Maple Lane",
-                            "Mahomet", "IL", "61853"));
+                "Mahomet", "IL", "61853"));
         ml.add(new Address("Ivan Ivanov", "867 Elm St",
                 "Champing", "IL", "61820"));
+
+        //Отобразить содержимое списка почтовых адресов
+        for (Address element : ml)
+            System.out.println(element + "\n");
+        System.out.println();
 
     }
 }
