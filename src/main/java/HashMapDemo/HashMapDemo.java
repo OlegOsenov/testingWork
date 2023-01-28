31eadeda1e11c4fefca46028401c312f5aeb2fc4
Oleg Osenov <olejka_oon@mail.ru>
2023-01-28 14:12:25 +0500

    Пополнить счет клиента

--- src/main/java/HashMapDemo/HashMapDemo.java
+++ src/main/java/HashMapDemo/HashMapDemo.java
@@ -19,6 +19,16 @@ public class HashMapDemo {
         // Получить набор элементов
         Set<Map .Entry<String, Double>> set = hm.entrySet();
 
+        //Отобразить содержимое набора
+        for (Map.Entry<String,Double> me: set){
+            System.out.print(me.getKey() + ": ");
+            System.out.println(me.getValue());
+        }
+        System.out.println();
+        // Пополнить счет клиента John Doe на 1000
+        double balance = hm.get("John Doe");
+        hm.put("John Doe", balance + 1000);
+        System.out.println("Новый баланс клиента John Doe: " + hm.get("John Doe"));
 
 
     }
