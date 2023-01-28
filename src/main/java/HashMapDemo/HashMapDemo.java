f37dbfd5be96cf820828b9586fc1f402625c0867
Oleg Osenov <olejka_oon@mail.ru>
2023-01-28 14:04:55 +0500

    Добавление элементов в хэшмап

--- src/main/java/HashMapDemo/HashMapDemo.java
+++ src/main/java/HashMapDemo/HashMapDemo.java
@@ -1,8 +1,20 @@
 package HashMapDemo;
 
+import java.util.HashMap;
+
 public class HashMapDemo {
 
     public static void main(String[] args) {
+
+        //Создать хэш карту
+        HashMap<String, Double> hm = new HashMap<>();
+        //Поместить элементы в карту
+        hm.put("John Doe", 3434.34);
+        hm.put("Tom Smith", 123.22);
+        hm.put("Jane Baker", 1378.00);
+        hm.put("Tod Hall", 99.22);
+
+
 
     }
 }
