f9518fbee376caf9819733777623ba131ba43da3
Oleg Osenov <olejka_oon@mail.ru>
2023-01-29 15:26:11 +0500

    Создать древовидный набор

---
+++ src/main/java/MyComp/CompDemo.java
@@ -0,0 +1,14 @@
+package MyComp;
+
+import java.util.TreeSet;
+
+public class CompDemo {
+
+    public static void main(String[] args) {
+
+        //Создать древовидный набор
+        TreeSet<String> ts = new TreeSet<>(new MyComp());
+
+    }
+
+}
