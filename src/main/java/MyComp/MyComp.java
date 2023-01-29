7400c531fad4f29f1da875a61dd2aab7147d7c19
Oleg Osenov <olejka_oon@mail.ru>
2023-01-29 15:24:26 +0500

    Сравнение для сортировки в обратном порядке

--- src/main/java/MyComp/MyComp.java
+++ src/main/java/MyComp/MyComp.java
@@ -1,5 +1,11 @@
 package MyComp;
 
-public class MyComp {
+import java.util.Comparator;
 
+class MyComp implements Comparator<String> {
+    @Override
+    public int compare(String aStr, String bStr) {
+        return bStr.compareTo(aStr);
+
+    }
 }
