74e5dfb3267a70a140682bebf390ebf4f01c0eef
Oleg Osenov <olejka_oon@mail.ru>
2023-01-31 20:43:07 +0500

    Сортировать список с использованием созданного компаратора

--- src/main/java/AlgorithmsDemo/AlgorithmsDemo.java
+++ src/main/java/AlgorithmsDemo/AlgorithmsDemo.java
@@ -1,5 +1,8 @@
 package AlgorithmsDemo;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedList;
 
 public class AlgorithmsDemo {
@@ -12,6 +15,13 @@ public class AlgorithmsDemo {
         ll.add(20);
         ll.add(-20);
         ll.add(8);
+
+        //Create a comparator with the reverse order
+        Comparator<Integer> r = Collections.reverseOrder();
+
+        //Sort the list using the created comparator
+        Collections.sort(ll,r);
+
 
     }
 
