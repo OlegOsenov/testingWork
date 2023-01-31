ca3bfed4c33048f42c75e1c4db37cff9551976f9
Oleg Osenov <olejka_oon@mail.ru>
2023-01-31 21:39:27 +0500

    Разместить в памяти и распределить массив. Отобразить, отсортировать и снова отобразить содержимое массива

--- src/main/java/ArraysDemo/ArraysDemo.java
+++ src/main/java/ArraysDemo/ArraysDemo.java
@@ -1,11 +1,29 @@
 package ArraysDemo;
 
+import java.util.Arrays;
+
 public class ArraysDemo {
 
     public static void main(String[] args) {
 
+        // Разместить в памяти и распределить массив
+        int[] array = new int[10];
+        for (int i = 0; i < 10; i++) {
+            array[i] = -3 * i;
+        }
+        // Отобразать, отсортировать и снова отобразить содержимое массива
+        System.out.print("Исходный массив: ");
+        display(array);
+        Arrays.sort(array);
+        System.out.print("Содержимое после сортировки: ");
+        display(array);
 
 
+    }
+    static void display(int[] array){
+        for (int i : array)
+            System.out.print(i + " ");
+        System.out.println();
     }
 
 
