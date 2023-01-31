c6c345ca543b292c0279c3c268d430040ac1a880
Oleg Osenov <olejka_oon@mail.ru>
2023-01-31 20:36:10 +0500

    Создать и инициализировать связный список

--- src/main/java/AlgorithmsDemo/AlgorithmsDemo.java
+++ src/main/java/AlgorithmsDemo/AlgorithmsDemo.java
@@ -1,6 +1,18 @@
 package AlgorithmsDemo;
 
+import java.util.LinkedList;
+
 public class AlgorithmsDemo {
 
+    public static void main(String[] args) {
+
+        //Создать и инициализировать связный список
+        LinkedList<Integer> ll = new LinkedList<>();
+        ll.add(-8);
+        ll.add(20);
+        ll.add(-20);
+        ll.add(8);
+
+    }
 
 }
