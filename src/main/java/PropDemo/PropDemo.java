a90109921b6b410b0bd2f3c2ae083ceea55c32de
Oleg Osenov <olejka_oon@mail.ru>
2023-01-31 22:37:33 +0500

    Демонстрация работы со списком свойств - КОНЕЦ

--- src/main/java/PropDemo/PropDemo.java
+++ src/main/java/PropDemo/PropDemo.java
@@ -1,8 +1,31 @@
 package PropDemo;
 
+import java.util.Objects;
+import java.util.Properties;
+import java.util.Set;
+
 public class PropDemo {
 
     public static void main(String[] args) {
 
+        Properties capitals = new Properties();
+        capitals.setProperty("Иллинойс", "Спрингфилд");
+        capitals.setProperty("Миссури", "Джефферсон-Сити");
+        capitals.setProperty("Вашингтон", "Олимпия");
+        capitals.setProperty("Калифорния", "Сакраменто");
+        capitals.setProperty("Индиана", "Индианаполис");
+        // Получить представление в виде набора для ключей
+        Set<?> states = capitals.keySet();
+        // Отобразить все штаты и их столицы
+        for (Object name : states) {
+            System.out.println("Столица штата " + name + " - " + capitals.getProperty((String) name) + ".");
+            System.out.println();
+        }
+        // Найти штат, отсутсвующий в списке - указать стандартные значение
+        String str = capitals.getProperty("Флорида", "не найдена");
+        System.out.println("Столица шатата Флориды - " + str);
+
     }
+
+
 }
