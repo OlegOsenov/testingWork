d5c545c0f88544d036801a71b8a301919658d25f
Oleg Osenov <olejka_oon@mail.ru>
2023-02-01 19:57:07 +0500

    Простая база данных телефонных номеров - НАЧАЛО

---
+++ src/main/java/PhoneBookTest/Phonebook.java
@@ -0,0 +1,22 @@
+package PhoneBookTest;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Properties;
+
+public class Phonebook {
+
+    public static void main(String[] args) {
+
+
+        Properties ht = new Properties();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in, System.console().charset()));
+
+
+
+    }
+
+
+
+
+}
