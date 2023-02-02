09ddf8615a23724fa3a174b888542fef4103dae8
Oleg Osenov <olejka_oon@mail.ru>
2023-02-02 21:04:51 +0500

    Создаем методот добавления сотрудников

--- src/main/java/PhoneBookV2/PhoneBook.java
+++ src/main/java/PhoneBookV2/PhoneBook.java
@@ -19,6 +19,17 @@ public class PhoneBook {
         PhoneBook pb = new PhoneBook();
         // Добавляем сотрудников и номера телефонов в список
         pb.add("Иванов", "8(909)345-54-56");
+        pb.add("Сидоров", "8(567)235-34-87");
+        pb.add("Петров", "8(234)596-21-32");
+        pb.add("Пунин", "8(567)567-65-54");
+        pb.add("Жиглов", "8(345)678-12-13");
+        pb.add("Иванов", "8(909)345-55-55");
+
+        // Проверка имен, если совпадает имя, вывести на экран все телефоны
+        for (String x: pb.get("Иванов")){
+            num++;
+            System.out.println("Пупкин " + num + " телефон: " + x);
+        }
 
 
     }
@@ -41,6 +52,10 @@ public class PhoneBook {
             phoneMap.put(surname, strings);
         }
 
+    }
+
+    public ArrayList<String> get(String surmane){
+        return phoneMap.get(surmane);
     }
 
 }
