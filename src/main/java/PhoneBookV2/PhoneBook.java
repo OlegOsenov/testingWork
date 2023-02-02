bdf2bedb8c72ae87081b40f77e92996b2c8f3274
Oleg Osenov <olejka_oon@mail.ru>
2023-02-02 20:52:47 +0500

    Создаем HashMap, который принимает уникальный ключ String и ArrayList<String>

--- src/main/java/PhoneBookV2/PhoneBook.java
+++ src/main/java/PhoneBookV2/PhoneBook.java
@@ -1,9 +1,25 @@
 package PhoneBookV2;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class PhoneBook {
 
 
     // Для цикла for, отображения кол-во номеров
     private static int num = 0;
+
+    // Создаем HashMap, который принимает уникальный ключ String и ArrayList<String>
+    HashMap<String, ArrayList<String>> phoneMap = new HashMap<>();
+
+    // Стандартный метод
+    public static void main(String[] args) {
+
+        // Инициализируем PhoneBook
+        PhoneBook pb = new PhoneBook();
+        // Добавляем сотрудников и номера телефонов в список
+
+
+    }
 
 }
