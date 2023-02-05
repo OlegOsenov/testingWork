4a1cd215974a84c862e75aff14649c7baf0dcac9
Oleg Osenov <olejka_oon@mail.ru>
2023-02-05 21:14:43 +0500

    Класс Person добавил переменную name, конструктор и getter

--- src/main/java/TreeMapDemoV/Person.java
+++ src/main/java/TreeMapDemoV/Person.java
@@ -1,4 +1,15 @@
 package TreeMapDemoV;
 
 public class Person {
+
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
 }
---
+++ src/main/java/TreeMapDemoV/TreeMapDemonstration.java
@@ -0,0 +1,4 @@
+package TreeMapDemoV;
+
+public class TreeMapDemonstration {
+}
