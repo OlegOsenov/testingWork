2b4dd292aca041ce5a01058bd86ec2bafa7c0658
Oleg Osenov <olejka_oon@mail.ru>
2023-02-05 21:23:00 +0500

    Получим объект по ключу 2

--- src/main/java/TreeMapDemoV/TreeMapDemonstration.java
+++ src/main/java/TreeMapDemoV/TreeMapDemonstration.java
@@ -1,4 +1,22 @@
 package TreeMapDemoV;
 
+import java.util.TreeMap;
+
 public class TreeMapDemonstration {
+
+    public static void main(String[] args) {
+
+        TreeMap<Integer, String> user = new TreeMap<>();
+        user.put(1, "Alex");
+        user.put(2, "Nik");
+        user.put(3, "Leo");
+        user.put(4, "John");
+
+        // Получим объект по ключу
+        System.out.println("Получим объект по ключу (2): " + user.get(2));
+
+
+    }
+
+
 }
