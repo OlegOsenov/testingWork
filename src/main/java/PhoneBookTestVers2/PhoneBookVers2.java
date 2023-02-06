64687acfb49539a1de2a020d70114675c5949385
Oleg Osenov <olejka_oon@mail.ru>
2023-02-06 22:01:03 +0500

    Заполнение класса PhoneBook, метод для добавления контактов

--- src/main/java/PhoneBookTestVers2/PhoneBookVers2.java
+++ src/main/java/PhoneBookTestVers2/PhoneBookVers2.java
@@ -1,7 +1,22 @@
 package PhoneBookTestVers2;
 
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
 public class PhoneBookVers2 {
 
+    private final Map<String, Set<String>> entries = new TreeMap<>();
+
+    private Set<String> getPhones(String surname){
+        return entries.getOrDefault(surname, new HashSet<>());
+    }
+
+    public void add(String surname, String phoneNumber){
+        Set<String> phones = getPhones(surname);
+        phones.add(phoneNumber);
+    }
 
 
 }
