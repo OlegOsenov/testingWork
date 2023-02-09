cea86423a5f66e0d19cc44514889f4d041d7ba29
Oleg Osenov <olejka_oon@mail.ru>
2023-02-09 20:14:30 +0500

    Конструктор класса Simple

--- src/main/java/gb_lesson_11/Sample01.java
+++ src/main/java/gb_lesson_11/Sample01.java
@@ -4,8 +4,23 @@ public class Sample01 {
 
     public static void main(String[] args) {
 
+        SimpleAccount simpleAccount = new SimpleAccount(111111, 500.1);
+
 
     }
+}
+class SimpleAccount{
 
+    private int id;
+    private double amount;
 
+    public SimpleAccount(int id, double amount) {
+        this.id = id;
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Сумма средств на счете #%s : %2f руб.", id, amount);
+    }
 }
