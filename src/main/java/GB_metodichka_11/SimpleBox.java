1ec2543af05c4dc48e7d04863a9dae61d63836f2
Oleg Osenov <olejka_oon@mail.ru>
2023-02-19 16:09:26 +0500

    Класс для записи любого типа данных

--- src/main/java/GB_metodichka_11/SimpleBox.java
+++ src/main/java/GB_metodichka_11/SimpleBox.java
@@ -1,7 +1,20 @@
 package GB_metodichka_11;
 
+
+
 public class SimpleBox {
 
+    private Object obj;
 
+    public SimpleBox(Object obj) {
+        this.obj = obj;
+    }
 
+    public Object getObj() {
+        return obj;
+    }
+
+    public void setObj(Object obj) {
+        this.obj = obj;
+    }
 }
