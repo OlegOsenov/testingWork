b5fa2c6dd8c3f9fe252f6219f747fb91fa3c9197
Oleg Osenov <olejka_oon@mail.ru>
2023-02-19 16:45:17 +0500

    Создание обощенного класса

--- src/main/java/GB_metodichka_11_2/TestGeneric.java
+++ src/main/java/GB_metodichka_11_2/TestGeneric.java
@@ -2,5 +2,17 @@ package GB_metodichka_11_2;
 
 public class TestGeneric<T> {
 
+    private T obj;
 
+    public T getObj() {
+        return obj;
+    }
+
+    public TestGeneric(T obj) {
+        this.obj = obj;
+    }
+
+    private void showType(){
+        System.out.println("Тип Т: " + obj.getClass().getName());
+    }
 }
