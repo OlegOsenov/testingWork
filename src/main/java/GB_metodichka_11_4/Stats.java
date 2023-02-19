feba34805fd809de00a5828bcf6803fc7e83a212
Oleg Osenov <olejka_oon@mail.ru>
2023-02-19 19:09:01 +0500

    Создал обобщенный класс

--- src/main/java/GB_metodichka_11_4/Stats.java
+++ src/main/java/GB_metodichka_11_4/Stats.java
@@ -1,7 +1,19 @@
 package GB_metodichka_11_4;
 
-public class Stats<T> {
+public class Stats<T extends Number> {
 
+    private T[] nums;
 
+    public Stats(T... nums) {
+        this.nums = nums;
+    }
+
+    public double avg() {
+        double sum = 0.0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i].doubleValue();
+        }
+        return sum / nums.length;
+    }
 
 }
