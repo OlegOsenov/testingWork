dc0a2d64ae3b7abd463831f6765a3e156fbdd258
Oleg Osenov <olejka_oon@mail.ru>
2023-02-19 16:14:57 +0500

    Если оба объекта Iteger то сложить их

--- src/main/java/GB_metodichka_11/BoxDemoApp.java
+++ src/main/java/GB_metodichka_11/BoxDemoApp.java
@@ -4,6 +4,15 @@ public class BoxDemoApp {
 
     public static void main(String[] args) {
 
+        SimpleBox intBox1 = new SimpleBox(20);
+        SimpleBox intBox2 = new SimpleBox(30);
+
+        if (intBox1.getObj() instanceof Integer && intBox2.getObj() instanceof Integer){
+            int sum = (Integer) intBox1.getObj() + (Integer) intBox2.getObj();
+            System.out.println("sum = " + sum);
+        } else {
+            System.out.println("Содержимое коробок отличается по типу");
+        }
 
 
 
