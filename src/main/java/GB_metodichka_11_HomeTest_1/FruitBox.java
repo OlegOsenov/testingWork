824e164d64f3ae676c5783533870a581e62da012
Oleg Osenov <olejka_oon@mail.ru>
2023-02-26 16:13:02 +0500

    FruitBox добавление логики

--- src/main/java/GB_metodichka_11_HomeTest_1/FruitBox.java
+++ src/main/java/GB_metodichka_11_HomeTest_1/FruitBox.java
@@ -1,5 +1,45 @@
 package GB_metodichka_11_HomeTest_1;
 
-public class FruitBox <F extends Fruit>{
+import java.security.PublicKey;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
+public class FruitBox<F extends Fruit> {
+
+    List<F> fruits;
+
+    public FruitBox(F... fruits) {
+        this.fruits = new ArrayList<>(Arrays.asList(fruits));
+    }
+
+    // Метод добавления
+    public void add(F fruit) {
+        this.fruits.add(fruit);
+    }
+
+    // Получение веса
+    public double getWeight() {
+        double weight = 0.0;
+        for (F fruit : fruits) {
+            weight += fruit.getWeight();
+        }
+        return weight;
+    }
+
+    public boolean equalsByWeight(FruitBox<?> another) {
+        return Math.abs(getWeight() - another.getWeight()) < 0.0001;
+    }
+
+    // Пересыпать фрукты в другую коробку
+    public void tranceferAll(FruitBox<? super F> another){
+        if (this == another) return;
+        another.getFruits().addAll(fruits);
+        fruits.clear();
+    }
+
+    public List<F> getFruits() {
+        return fruits;
+    }
 }
