07d2bd011f5086f88d8a5717ceab95298fc32025
Oleg Osenov <olejka_oon@mail.ru>
2023-02-26 20:19:47 +0500

    Многопоточка class MyThread c коструктором

--- src/main/java/GB_metodichka_12/Classwork.java
+++ src/main/java/GB_metodichka_12/Classwork.java
@@ -5,13 +5,18 @@ public class Classwork {
 
         System.out.println("Hello from main!!!, Thread  name is " + Thread.currentThread().getName());
 
-        Thread t1 = new MyThread();
+        Thread t1 = new MyThread("MyThread");
         t1.start();
 
 
     }
 
     static class MyThread extends Thread{
+
+        public MyThread(){}
+        public MyThread(String name){
+            super(name);
+        }
         @Override
         public void run() {
             System.out.println("Hello from MyThread!!!, Thread  name is " + Thread.currentThread().getName());
