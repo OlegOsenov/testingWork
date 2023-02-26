d132b2804caf33df50224330e9ae796066aa610d
Oleg Osenov <olejka_oon@mail.ru>
2023-02-26 20:04:51 +0500

    Многопоточка class MyThread

--- src/main/java/GB_metodichka_12/Classwork.java
+++ src/main/java/GB_metodichka_12/Classwork.java
@@ -3,5 +3,20 @@ package GB_metodichka_12;
 public class Classwork {
     public static void main(String[] args) {
 
+        System.out.println("Hello from main!!!, Thread  name is " + Thread.currentThread().getName());
+
+        Thread t1 = new MyThread();
+        t1.start();
+
+
     }
+
+    static class MyThread extends Thread{
+        @Override
+        public void run() {
+            System.out.println("Hello from MyThread!!!, Thread  name is " + Thread.currentThread().getName());
+        }
+    }
+
+
 }
