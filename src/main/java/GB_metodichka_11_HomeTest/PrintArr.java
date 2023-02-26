9d13cfdb3a74b4f1c1de4403abaf222a704052fa
Oleg Osenov <olejka_oon@mail.ru>
2023-02-26 12:36:39 +0500

    // 2. Написать метод, который преобразует массив в ArrayList;

--- src/main/java/GB_metodichka_11_HomeTest/PrintArr.java
+++ src/main/java/GB_metodichka_11_HomeTest/PrintArr.java
@@ -1,28 +1,41 @@
 package GB_metodichka_11_HomeTest;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class PrintArr {
 
 
-
     public static void main(String[] args) {
 
+        System.out.println("Задание 1: ");
         String[] array = {"a", "b", "c", "d"};
         Integer[] number = {1, 2, 3, 4, 5};
-        swapArr(array,1,2);
+        swapArr(array, 1, 2);
         System.out.println(Arrays.toString(array));
-        swapArr(number,1,2);
+        swapArr(number, 1, 2);
         System.out.println(Arrays.toString(number));
+
+        System.out.println("Задание 2: ");
+        String[] arrayStr = {"apple", "beer", "cola", "door"};
+        System.out.println("arrayStr: " + Arrays.toString(arrayStr));
+        List<String> list = convertToArrayAsList(arrayStr);
+        System.out.println("list: " + list);
 
 
     }
 
     // 1. Написать метод, который меняет два элемента массива местами (массив может быть любого ссылочного типа);
-    private static  <T> void swapArr(T[] arr, int index1, int index2) {
+    private static <T> void swapArr(T[] arr, int index1, int index2) {
         T temp = arr[index1];
         arr[index1] = arr[index2];
         arr[index2] = temp;
+    }
+
+    // 2. Написать метод, который преобразует массив в ArrayList;
+    public static <T>List<T> convertToArrayAsList(T array[]){
+        List<T> list = Arrays.asList(array);
+        return list;
     }
 
 }
