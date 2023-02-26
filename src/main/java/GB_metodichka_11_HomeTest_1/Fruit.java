6b1c9d1d5e6dbd02a9058f4197b6fd03b788f360
Oleg Osenov <olejka_oon@mail.ru>
2023-02-26 15:49:29 +0500

    конструктор Фруктов

--- src/main/java/GB_metodichka_11_HomeTest_1/Fruit.java
+++ src/main/java/GB_metodichka_11_HomeTest_1/Fruit.java
@@ -1,4 +1,18 @@
 package GB_metodichka_11_HomeTest_1;
 
 public class Fruit {
+
+    float weight;
+
+    public Fruit(float weight) {
+        this.weight = weight;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
 }
