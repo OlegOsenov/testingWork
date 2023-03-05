06293e2f1fe5d5b45bb9cb0a9f1b15801274e299
Oleg Osenov <olejka_oon@mail.ru>
2023-03-05 15:20:52 +0500

    Thread.sleep

--- src/main/java/GB_metodichka_12/Classwork.java
+++ src/main/java/GB_metodichka_12/Classwork.java
@@ -9,20 +9,32 @@ public class Classwork {
         System.out.println(thread.getName());
         System.out.println(thread.getPriority());
 
+        MyThread myThread01 = new MyThread("thread #1");
+        myThread01.start();
+
         System.out.println("Завершение главного потока ...");
 
 
     }
 
-    static class MyThread extends Thread{
+    static class MyThread extends Thread {
 
-        public MyThread(){}
-        public MyThread(String name){
+        public MyThread() {
+        }
+
+        public MyThread(String name) {
             super(name);
         }
+
         @Override
         public void run() {
-            System.out.println("Hello from MyThread!!!, Thread  name is " + Thread.currentThread().getName());
+            System.out.printf("Поток %s запущен \n", Thread.currentThread().getName());
+            try {
+                Thread.sleep(1500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.printf("Поток %s завершил свою работу \n", Thread.currentThread().getName());
         }
     }
 
