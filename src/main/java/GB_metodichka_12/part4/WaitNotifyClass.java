8ce4c179332eae3b668d337bc3c4dab85f66286e
Oleg Osenov <olejka_oon@mail.ru>
2023-03-06 22:04:27 +0500

    Создание методов печати A и B

--- src/main/java/GB_metodichka_12/part4/WaitNotifyClass.java
+++ src/main/java/GB_metodichka_12/part4/WaitNotifyClass.java
@@ -7,6 +7,52 @@ public class WaitNotifyClass {
 
     public static void main(String[] args) {
 
+        WaitNotifyClass w = new WaitNotifyClass();
+        Thread t1 = new Thread(() -> {
+            w.printA();
+        });
+        Thread t2 = new Thread(()->{
+            w.prinB();
+        });
+        t1.start();
+        t2.start();
+
+        }
+
+    private void prinB() {
+        synchronized (mon){
+            try {
+                for (int i = 0; i < 3; i++) {
+                    while (currentLetter != 'B'){
+                        mon.wait();
+                    }
+                    System.out.print("B");
+                    currentLetter = 'A';
+                    mon.notify();
+                }
+            }
+            catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void printA() {
+        synchronized (mon){
+            try {
+                for (int i = 0; i < 3; i++) {
+                    while (currentLetter != 'A'){
+                        mon.wait();
+                    }
+                    System.out.print("A");
+                    currentLetter = 'B';
+                    mon.notify();
+                }
+            }
+            catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
 
     }
 
