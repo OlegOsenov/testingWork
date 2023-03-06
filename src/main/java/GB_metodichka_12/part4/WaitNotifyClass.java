e6e52a798e60ad9f3bdb419536179e80b7ad5765
Oleg Osenov <olejka_oon@mail.ru>
2023-03-06 21:55:49 +0500

    class WaitNotifyClass добавление переменных в класс

--- src/main/java/GB_metodichka_12/part4/WaitNotifyClass.java
+++ src/main/java/GB_metodichka_12/part4/WaitNotifyClass.java
@@ -2,4 +2,13 @@ package GB_metodichka_12.part4;
 
 public class WaitNotifyClass {
 
+    private final Object mon = new Object();
+    private volatile char currentLetter = 'A';
+
+    public static void main(String[] args) {
+
+
+    }
+
+
 }
