eeadae3c621dbb4bb8ee860b20f4b2888d6ba3eb
Oleg Osenov <olejka_oon@mail.ru>
2023-03-10 21:53:58 +0500

    Создал два класса ThreadOne и ThreadTwo

--- src/main/java/GB_metodichka_12/part5/ExampleDeadlock.java
+++ src/main/java/GB_metodichka_12/part5/ExampleDeadlock.java
@@ -7,6 +7,43 @@ public class ExampleDeadlock {
 
     public static void main(String[] args) {
 
+
+
+    }
+
+
+    private static class ThreadOne extends Thread{
+        public void run(){
+            synchronized (lock1){
+                System.out.println("Thread захватил lock1");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                System.out.println("Thread ожидает lock2");
+                synchronized (lock2){
+                    System.out.println("Thread1 захватил lock1 и lock2");
+                }
+            }
+        }
+    }
+
+    private static class ThreadTwo extends Thread{
+        public void run(){
+            synchronized (lock2){
+                System.out.println("Thread2 захватил lock2");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                System.out.println("Thread2 ожидает lock1");
+                synchronized (lock1){
+                    System.out.println("Thread захватил lock1 и lock2");
+                }
+            }
+        }
     }
 
 }
