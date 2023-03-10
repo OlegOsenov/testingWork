95f5be1906b0d2fd453ff63b57516ace9c0171db
Oleg Osenov <olejka_oon@mail.ru>
2023-03-10 21:47:04 +0500

    Создание двух переменных - мониторов

--- src/main/java/GB_metodichka_12/part5/ExampleDeadlock.java
+++ src/main/java/GB_metodichka_12/part5/ExampleDeadlock.java
@@ -2,4 +2,11 @@ package GB_metodichka_12.part5;
 
 public class ExampleDeadlock {
 
+    private static final Object lock1 = new Object();
+    private static final Object lock2 = new Object();
+
+    public static void main(String[] args) {
+
+    }
+
 }
