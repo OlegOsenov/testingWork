3e20b2e883e773636dbf98f3c71c6f24de8f091c
Oleg Osenov <olejka_oon@mail.ru>
2023-03-10 22:07:13 +0500

    Переопределение метода run()

--- src/main/java/GB_metodichka_12/part6/RunnableDemo.java
+++ src/main/java/GB_metodichka_12/part6/RunnableDemo.java
@@ -1,5 +1,27 @@
 package GB_metodichka_12.part6;
 
 public class RunnableDemo {
-    boolean suspended = false;
+    static class RunnableClass implements Runnable{
+        boolean suspended = false;
+
+        @Override
+        public void run() {
+            System.out.println("Запуск потока");
+            try {
+                for (int i = 10; i > 0; i++) {
+                    System.out.println(i);
+                    Thread.sleep(300);
+                    synchronized (this){
+                        while (suspended){
+                            wait();
+                        }
+                    }
+                }
+            } catch (InterruptedException e){
+                System.out.println("Поток прерван");
+            }
+            System.out.println("Завершение потока");
+        }
+    }
+
 }
