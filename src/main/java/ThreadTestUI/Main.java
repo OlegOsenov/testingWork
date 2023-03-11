ccc18f20bd696f3e7c6a0bcb4e455d1401273fc5
Oleg Osenov <olejka_oon@mail.ru>
2023-03-11 20:55:28 +0500

    Третий способ потока

--- src/main/java/ThreadTestUI/Main.java
+++ src/main/java/ThreadTestUI/Main.java
@@ -9,11 +9,34 @@ public class Main {
         Elements obj2 = new Elements();
         obj2.start();
 */
-        Thread obj3 = new Thread(new NewTest());
+    /*    Thread obj3 = new Thread(new NewTest());
         obj3.start();
 
         Thread obj4 = new Thread(new NewTest());
         obj4.start();
+*/
+        /*
+        Thread obj5 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 1; i <= 10; i++) {
+                    System.out.println("Элемент: " + i);
+                }
+            }
+        });
+        obj5.start();
+
+        Thread obj6 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 1; i <= 10; i++) {
+                    System.out.println("Элемент: " + i);
+                }
+            }
+        });
+        obj6.start();
+
+*/
 
     }
 
