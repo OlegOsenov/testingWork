08ef35ee8a9faf821c2b8620ab25726c4644b9b9
Oleg Osenov <olejka_oon@mail.ru>
2023-03-11 20:45:23 +0500

    Создание объектов Elements и запуск потоков

--- src/main/java/ThreadTestUI/Elements.java
+++ src/main/java/ThreadTestUI/Elements.java
@@ -3,7 +3,7 @@ package ThreadTestUI;
 public class Elements extends Thread {
     @Override
     public void run() {
-        for (int i = 1; i < 10; i++) {
+        for (int i = 1; i <= 10; i++) {
             System.out.println("Элемент: " + i);
         }
     }
--- src/main/java/ThreadTestUI/Main.java
+++ src/main/java/ThreadTestUI/Main.java
@@ -1,4 +1,16 @@
 package ThreadTestUI;
 
 public class Main {
+
+    public static void main(String[] args) {
+        Elements obj1 = new Elements();
+        obj1.start();
+
+        Elements obj2 = new Elements();
+        obj2.start();
+
+
+    }
+
+
 }
