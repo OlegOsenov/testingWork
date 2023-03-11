ccfdc5fff177429863be3f5627e4d2065bf21642
Oleg Osenov <olejka_oon@mail.ru>
2023-03-11 20:50:26 +0500

    Создание класса NewTest implements Runnable

--- src/main/java/ThreadTestUI/Main.java
+++ src/main/java/ThreadTestUI/Main.java
@@ -3,12 +3,17 @@ package ThreadTestUI;
 public class Main {
 
     public static void main(String[] args) {
-        Elements obj1 = new Elements();
+       /* Elements obj1 = new Elements();
         obj1.start();
 
         Elements obj2 = new Elements();
         obj2.start();
+*/
+        Thread obj3 = new Thread(new NewTest());
+        obj3.start();
 
+        Thread obj4 = new Thread(new NewTest());
+        obj4.start();
 
     }
 
