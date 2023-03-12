4f29e3525cab28d5c1b555da9e7adcc88f8887b1
Oleg Osenov <olejka_oon@mail.ru>
2023-03-12 16:21:47 +0500

    Первый способ создания потока через наследование

--- src/main/java/ThreadTestUI/ThreadV1/HelloWorld.java
+++ src/main/java/ThreadTestUI/ThreadV1/HelloWorld.java
@@ -1,4 +1,16 @@
 package ThreadTestUI.ThreadV1;
 
 public class HelloWorld {
+
+    public static class MyThread extends Thread {
+        @Override
+        public void run() {
+            System.out.println("Hello world!");
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread thread = new MyThread();
+        thread.start();
+    }
 }
