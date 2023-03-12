7f032d0f18a9ffe2903a57b3f30004f7c3765206
Oleg Osenov <olejka_oon@mail.ru>
2023-03-12 16:32:22 +0500

    Второй способ создания потока через интерфейс

--- src/main/java/ThreadTestUI/ThreadV2/ThreadV2.java
+++ src/main/java/ThreadTestUI/ThreadV2/ThreadV2.java
@@ -2,4 +2,15 @@ package ThreadTestUI.ThreadV2;
 
 public class ThreadV2 {
 
+    public static void main(String[] args) {
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Hello world!");
+            }
+        };
+        Thread thread = new Thread(task);
+        thread.start();
+    }
+
 }
