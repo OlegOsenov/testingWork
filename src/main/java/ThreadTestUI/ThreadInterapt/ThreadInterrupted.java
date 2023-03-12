3049fa3c939240d40e1549827a738d6f5f7c9508
Oleg Osenov <olejka_oon@mail.ru>
2023-03-12 19:56:46 +0500

    ThreadInterrupted DONE

--- src/main/java/ThreadTestUI/ThreadInterapt/ThreadInterrupted.java
+++ src/main/java/ThreadTestUI/ThreadInterapt/ThreadInterrupted.java
@@ -1,5 +1,21 @@
 package ThreadTestUI.ThreadInterapt;
 
+import java.util.concurrent.TimeUnit;
+
 public class ThreadInterrupted {
+
+    public static void main(String[] args) {
+        Runnable task = () -> {
+            try {
+                TimeUnit.SECONDS.sleep(60);
+            } catch (InterruptedException e) {
+                System.out.println("Interrupted");
+            }
+        };
+
+        Thread thread = new Thread(task);
+        thread.start();
+        thread.interrupt();
+    }
 
 }
