3a268af5c33fe8bb7d48d678e24e29b3af469215
Oleg Osenov <olejka_oon@mail.ru>
2023-03-12 16:00:43 +0500

    ThreadException

--- src/main/java/ThreadTestUI/ThreadGroup/ThreadException/ThreadException.java
+++ src/main/java/ThreadTestUI/ThreadGroup/ThreadException/ThreadException.java
@@ -3,5 +3,14 @@ package ThreadTestUI.ThreadGroup.ThreadException;
 public class ThreadException {
     public static void main(String[] args) {
 
+        Thread th = Thread.currentThread();
+        th.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                System.out.println("Возникла ошибка: " + e.getMessage());
+            }
+        });
+        System.out.println(2/0);
+
     }
 }
