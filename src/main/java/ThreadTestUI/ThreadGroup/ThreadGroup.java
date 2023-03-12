54f6bd63d2528fbf2d149e2eb5bdd9678bc58015
Oleg Osenov <olejka_oon@mail.ru>
2023-03-12 15:54:31 +0500

    ThreadGroup

--- src/main/java/ThreadTestUI/ThreadGroup/ThreadGroup.java
+++ src/main/java/ThreadTestUI/ThreadGroup/ThreadGroup.java
@@ -1,4 +1,12 @@
 package ThreadTestUI.ThreadGroup;
-
 public class ThreadGroup {
+
+    public static void main(String[] args) {
+        Thread currentThread = Thread.currentThread();
+        java.lang.ThreadGroup threadGroup = currentThread.getThreadGroup();
+        System.out.println("Thread: " + currentThread.getName());
+        System.out.println("Thread Group: " + threadGroup.getName());
+        System.out.println("Parent Group: " + threadGroup.getParent().getName());
+    }
+
 }
