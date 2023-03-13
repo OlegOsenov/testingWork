c65e36bfcc73764154603ba631131fea2657cff6
Oleg Osenov <olejka_oon@mail.ru>
2023-03-13 22:11:27 +0500

    Wait и ожидание по монитору - Конец

--- src/main/java/ThreadTestUI/ThreadWait/ThreadWait.java
+++ src/main/java/ThreadTestUI/ThreadWait/ThreadWait.java
@@ -1,8 +1,30 @@
 package ThreadTestUI.ThreadWait;
 
 public class ThreadWait {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
+        Object lock = new Object();
+        /* task будет ждать пока его не оповестят через lock*/
+        Runnable task = () -> {
+            synchronized (lock) {
+                try {
+                    lock.wait();
+                } catch (InterruptedException e) {
+                    System.out.println("interrupted");
+                }
+            }
+//            После оповещения нас мы будем ждать, пока сможем взять lock
+            System.out.println("thread");
+        };
+
+        Thread taskThread = new Thread(task);
+        taskThread.start();
+//        Ждем и после этого забераем себе lock, оповещаем и отдаем лок
+        Thread.currentThread().sleep(3000);
+        System.out.println("main");
+        synchronized (lock) {
+            lock.notify();
+        }
     }
 
 }
