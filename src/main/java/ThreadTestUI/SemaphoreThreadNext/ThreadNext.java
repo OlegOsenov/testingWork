fd988848fd09c9979b106b5473539f6316128cb4
Oleg Osenov <olejka_oon@mail.ru>
2023-03-14 21:57:55 +0500

    Конец

--- src/main/java/ThreadTestUI/SemaphoreThreadNext/ThreadNext.java
+++ src/main/java/ThreadTestUI/SemaphoreThreadNext/ThreadNext.java
@@ -3,12 +3,24 @@ package ThreadTestUI.SemaphoreThreadNext;
 import java.util.concurrent.locks.LockSupport;
 
 public class ThreadNext {
-    Runnable task = () -> {
+
+    public static void main(String[] args) throws InterruptedException {
+        Runnable task = () -> {
 //        Запаркуем текущий поток
-        System.err.println("Will be Parked");
-        LockSupport.park();
+            System.err.println("Will be Parked");
+            LockSupport.park();
 //        Как только нас распаркуют - начнем действовать
-        System.err.println("Unparked");
-    };
+            System.err.println("Unparked");
+        };
+        Thread th = new Thread(task);
+        th.start();
+        Thread.currentThread().sleep(2000);
+        System.err.println("Thread state: " + th.getState());
+
+        LockSupport.unpark(th);
+        Thread.currentThread().sleep(2000);
+
+    }
+
 
 }
