b70c2391a476681b46a208274c0ba471b44de05d
Oleg Osenov <olejka_oon@mail.ru>
2023-03-14 21:50:31 +0500

    Парковка потоков КОНЕЦ

--- src/main/java/ThreadTestUI/Semaphore/ThreadSemafore.java
+++ src/main/java/ThreadTestUI/Semaphore/ThreadSemafore.java
@@ -1,6 +1,21 @@
 package ThreadTestUI.Semaphore;
 
+import java.util.concurrent.Semaphore;
+
 public class ThreadSemafore {
+
+    public static void main(String[] args) {
+
+        Semaphore semaphore = new Semaphore(0);
+        try {
+            semaphore.acquire();
+        } catch (InterruptedException e) {
+//            Просим разрешение и ждем, пока не получим его
+            e.printStackTrace();
+        }
+        System.out.println("Hello world!");
+
+    }
 
 
 
