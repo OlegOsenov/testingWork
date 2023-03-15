3222f2e8340f90c0e15f609618753311b9a0b5f3
Oleg Osenov <olejka_oon@mail.ru>
2023-03-15 21:35:39 +0500

    class Friend

--- src/main/java/ThreadTestUI/ThreadDeedlock/ThDeedlock.java
+++ src/main/java/ThreadTestUI/ThreadDeedlock/ThDeedlock.java
@@ -2,6 +2,28 @@ package ThreadTestUI.ThreadDeedlock;
 
 public class ThDeedlock {
 
+    static class Friend {
+        private final String name;
+
+        Friend(String name) {
+            this.name = name;
+        }
+
+        String getName() {
+            return this.name;
+        }
+
+        public synchronized void bow(Friend bower) {
+            System.out.format("%s: %s has bowed to me!%n", this.name, bower.getName());
+            bower.bowBack(this);
+        }
+
+        public synchronized void bowBack(Friend bower) {
+            System.out.format("%s: %s has bowed back to me!%n", this.name, bower.getName());
+        }
+
+
+    }
 
 
 }
