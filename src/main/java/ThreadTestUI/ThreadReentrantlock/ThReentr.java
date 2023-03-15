9ddad0d35cb97303808f7e618ce55ce96bee14a9
Oleg Osenov <olejka_oon@mail.ru>
2023-03-15 21:08:11 +0500

    ReentrantLock конец

--- src/main/java/ThreadTestUI/ThreadReentrantlock/ThReentr.java
+++ src/main/java/ThreadTestUI/ThreadReentrantlock/ThReentr.java
@@ -1,6 +1,26 @@
 package ThreadTestUI.ThreadReentrantlock;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class ThReentr {
 
+    public static void main(String[] args) throws InterruptedException {
+
+        Lock lock = new ReentrantLock();
+        Runnable task = () -> {
+            lock.lock();
+            System.out.println("Thread");
+            lock.unlock();
+        };
+        lock.lock();
+
+        Thread th = new Thread(task);
+        th.start();
+        System.out.println("main");
+        Thread.currentThread().sleep(2000);
+        lock.unlock();
+
+    }
 
 }
