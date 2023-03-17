6c3631d82f3b9cb9d63e06c14c19495491da3256
Oleg Osenov <olejka_oon@mail.ru>
2023-03-17 22:08:25 +0500

    Состояние гонки Конец

--- src/main/java/ThreadTestUI/ThreadRaceCondition/RaceCondtApp.java
+++ src/main/java/ThreadTestUI/ThreadRaceCondition/RaceCondtApp.java
@@ -2,4 +2,23 @@ package ThreadTestUI.ThreadRaceCondition;
 
 public class RaceCondtApp {
 
+    public static int value = 0;
+
+    public static void main(String[] args) {
+        Runnable task = () -> {
+            for (int i = 0; i < 10000; i++) {
+                int oldValue = value;
+                int newValue = ++value;
+                if (oldValue + 1 != newValue){
+                    throw new IllegalArgumentException(oldValue + " + 1 = " + newValue);
+                }
+            }
+        };
+
+        new Thread(task).start();
+        new Thread(task).start();
+        new Thread(task).start();
+
+    }
+
 }
