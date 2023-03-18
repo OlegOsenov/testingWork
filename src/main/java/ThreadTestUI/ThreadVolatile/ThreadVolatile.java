730da4d0f845b090cd80c52893843e9d387f00c4
Oleg Osenov <olejka_oon@mail.ru>
2023-03-18 15:13:51 +0500

    Взаймодействие потоков Volatile НАЧАЛО

--- src/main/java/ThreadTestUI/ThreadVolatile/ThreadVolatile.java
+++ src/main/java/ThreadTestUI/ThreadVolatile/ThreadVolatile.java
@@ -2,5 +2,18 @@ package ThreadTestUI.ThreadVolatile;
 
 public class ThreadVolatile {
 
+    public static boolean flag = false;
+
+    public static void main(String[] args) throws InterruptedException {
+        Runnable whileFlagFalse = () -> {
+            while (!flag){
+            }
+            System.out.println("Flag is now TRUE");
+        };
+
+        new Thread(whileFlagFalse).start();
+        Thread.sleep(1000);
+        flag = true;
+    }
 
 }
